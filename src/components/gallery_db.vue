fbe57175a9d2f322f4ececa5b55a2323b1ab03d7
hanifaja <nurdiyantor21064@gmail.com>
2022-12-12 15:33:20 +0700

    gallery

--- src/components/gallery_db.vue
+++ src/components/gallery_db.vue
@@ -2,7 +2,7 @@
     <div class="relative">
         <!-- start gallery -->
     <div class="px-16">
-    <div class="grid grid-rows-3 grid-flow-col ">
+    <div class="grid grid-rows-3">
         <div class="w-72 h-72 bg-slate-700 rounded-lg overflow-hidden">
           <img class="object-cover" :src="gallery.image" alt="">
         </div>
