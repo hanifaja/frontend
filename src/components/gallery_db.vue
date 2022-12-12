28396f7edb84d597a19ada9729ff364e931e53fa
hanifaja <nurdiyantor21064@gmail.com>
2022-12-12 12:49:18 +0700

    galler_db

--- src/components/gallery_db.vue
+++ src/components/gallery_db.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="relative">
         <!-- start gallery -->
-    <div class="px-16 grid grid-rows-3">
-    <div class="grid grid-cols-3">
+    <div class="px-16">
+    <div class="grid grid-rows-3 grid-flow-col ">
         <div class="w-72 h-72 bg-slate-700 rounded-lg overflow-hidden">
           <img class="object-cover" :src="gallery.image" alt="">
         </div>
