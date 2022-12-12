cc1e0acbecc23cbdeabcd24b791b55955e09c873
hanifaja <nurdiyantor21064@gmail.com>
2022-12-12 09:39:35 +0700

    init commit

---
+++ src/router/index.js
@@ -0,0 +1,68 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/home_view.vue'
+import programing from '../views/programing_view.vue'
+import dimensi from '../views/3D_view.vue'
+import ilustrasi from '../views/ilustrasi_view.vue'
+import presentasi from '../views/presentasi_view.vue'
+// import detail_karya from '../views/detail_karya_view.vue'
+import login from '../views/login_view.vue'
+import gallery from '../views/gallery_view.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
+    {
+      path: '/about',
+      name: 'about',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/about_view.vue')
+    },
+    {
+      path: '/programing',
+      name: 'programing',
+      component: programing
+    },
+    {
+      path: '/3D',
+      name: 'dimensi',
+      component: dimensi
+    },
+    {
+      path: '/ilustrasi',
+      name: 'ilustrasi',
+      component: ilustrasi
+    },
+    {
+      path: '/presentasi',
+      name: 'presentasi',
+      component: presentasi
+    },
+    // {
+    //   path: '/detail-karya',
+    //   name: 'detail-karya',
+    //   component: detail_karya
+    // },
+    {
+      path: '/login',
+      name: 'login',
+      component: login
+    },
+    {
+      path: '/gallery',
+      name: 'gallery',
+      component: gallery
+    },
+  ],
+    scrollBehavior() {
+       document.getElementById('app').scrollIntoView({behavior:'smooth'});
+    }
+})
+
+export default router
